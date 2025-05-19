4d1115b95a4d3b586b13eca262d0157bdb437579
Dennis <Dennissvarts@hotmail.com>
2025-05-19 11:30:09 +0200

    update azure

--- InvoiceService/Program.cs
+++ InvoiceService/Program.cs
@@ -3,47 +3,41 @@ using Microsoft.EntityFrameworkCore;
 using InvoiceService.Repositories;
 using InvoiceService.Services;
 
-
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(option =>
+builder.Services.AddCors(options =>
 {
-    option.AddPolicy("AllowFrontend",
-         policy =>
-         {
-             policy.WithOrigins("https://localhost:7080")
-                 .AllowAnyMethod()
-                 .AllowAnyHeader();
-         });
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+            "https://localhost:7080",
+            "https://ventixe-frontend.azurewebsites.net"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
 });
 
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
-builder.Services.AddScoped<InvoiceService.Services.IInvoiceService, InvoiceService.Services.InvoiceService>();
-
-
+builder.Services.AddScoped<IInvoiceService, InvoiceService.Services.InvoiceService>();
 
 builder.Services.AddDbContext<InvoiceDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("InvoiceConnection")));
 
-
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
